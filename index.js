bd25bca56ee791ae20625b8097948aec9054eede
Isak05 <isak.wecksten@gmail.com>
2025-05-26 12:04:33 +0200

    test

--- index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const app = express();
-
-app.get(/(.*)/, (req, res) => res.send("Express on Vercel"));
-
-app.listen(80, () => console.log("Server ready on port 80."));
-
-export default app
